e98b299d3a73332430e23718433185588c58586e
AlejandroPech <66346773+AlejandroPech@users.noreply.github.com>
2022-11-10 10:38:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 //redireccionar a la vista de loginss
+header('Access-Control-Allow-Origin: ' . (isset($_SERVER['HTTP_REFERER']) ? $REFERER['scheme'].'://'.$REFERER['host'] . (isset($REFERER['port']) ? (':' . $REFERER['port']) : '') : '*'));
 $uriData = parse_url($_SERVER['REQUEST_URI']);
 header ('Location: '.$uriData["scheme"].'/view/login.html');
 ?>
