25458416bbaa962eb19499feccb5c6ca9c9ad0d4
Alejandro <jesuspech511@gmail.com>
2022-11-10 14:47:36 -0600

    cambio cadena de conexion

--- config/global.php
+++ config/global.php
@@ -3,6 +3,7 @@ define("DB_HOST","containers-us-west-114.railway.app");
 define("DB_NAME", "railway");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "8NhpSS3zWFd6bcrbkD0I");
+define("DB_PORT","6515");
 define("DB_ENCODE","utf8");
 define("PRO_NOMBRE","railway");
 ?>
