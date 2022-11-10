0694e335b4741481b86dba69c23dd894d8ea9eae
Alejandro <jesuspech511@gmail.com>
2022-11-10 13:40:01 -0600

    correccion redirect

--- view/index.php
+++ view/index.php
@@ -1,5 +1,4 @@
 <?php 
 //redireccionar a la vista de loginss
-$uriData = parse_url($_SERVER['REQUEST_URI']);
-header ('Location: '.$uriData["scheme"].'/view/login.html');
+header ('Location: /view/login.html');
 ?>
