0d3a3e1dbcb8857e27ffa5ca11bcdfd11a3d2068
Alejandro <jesuspech511@gmail.com>
2022-11-11 11:19:28 -0600

    service-worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'view/',
 	globPatterns: [
-		'**/*.{php,css,jpg,png,js,html,txt,eot,svg,ttf,woff,woff2,ico,json,otf,pdf}'
+		'**/*.{css,jpg,png,js,html,txt,eot,svg,ttf,woff,woff2,ico,json,otf,pdf}'
 	],
 	swDest: 'view/sw.js',
 	ignoreURLParametersMatching: [
