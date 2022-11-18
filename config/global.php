304aad305b9d89cc106a3ccb1cef8b7e74e80c99
Alejandro <jesuspech511@gmail.com>
2022-11-18 11:46:21 -0600

    cambio conexion

--- config/global.php
+++ config/global.php
@@ -1,9 +1,9 @@
 <?php 
-define("DB_HOST","192.168.19.93");
-define("DB_NAME", "bs");
-define("DB_USERNAME", "developer");
-define("DB_PASSWORD", "D3v2021");
-define("DB_PORT","3306");
+define("DB_HOST","containers-us-west-114.railway.app");
+define("DB_NAME", "railway");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "8NhpSS3zWFd6bcrbkD0I");
+define("DB_PORT","6515");
 define("DB_ENCODE","utf8");
 define("PRO_NOMBRE","railway");
 ?>
