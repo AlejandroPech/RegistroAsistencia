72cd0a2629525138adcf356bdebe532063583d82
Alejandro <jesuspech511@gmail.com>
2022-11-18 09:34:45 -0600

    correccion update incidencias

--- model/Incidencia.php
+++ model/Incidencia.php
@@ -16,22 +16,26 @@ Class Incidencia
     }
 
     public function editarincidencia($idincidencia,$observacion,$idhorario,$num_usuario,$ubicacion,$fecha_registro){
-        $sql = "update incidencias set observacion = '$observacion', fecha_atendida = NOW(), fecha_modificacion = NOW(), latendida = 1 
-        where idincidencia = '$idincidencia'";
-        $respuesta = ejecutarConsulta($sql);
+        $sqlupdate = "";
         if($idhorario == "" || $idhorario == "0"){
-            $sql="INSERT INTO horario (fecha_registro,ubicacion,num_usuario,fecha_creacion,fecha_modificacion,lactivo)
-            VALUES ('$fecha_registro','$ubicacion','$num_usuario',NOW(),null,1)";
+            $sql="INSERT INTO horario (fecha_registro,ubicacion,num_usuario,imagen,fecha_creacion,fecha_modificacion,lactivo)
+            VALUES ('$fecha_registro','$ubicacion','$num_usuario','',NOW(),null,1);";
             $respuesta = ejecutarConsulta_retornarID($sql);
-
-            $sql = "update incidencias set observacion = '$observacion', fecha_atendida = NOW(), fecha_modificacion = NOW(), latendida = 1 
-            where idincidencia = '$idincidencia'";
-            $respuesta = ejecutarConsulta($sql);
+            $sqlupdate = ", idhorario = '$respuesta'";
         }else{
             $sql = "update horario set fecha_registro = '$fecha_registro', ubicacion = '$ubicacion', fecha_modificacion = NOW(), latendida = 1 
             where idhorario = '$idhorario'";
             $respuesta = ejecutarConsulta($sql);
         }
+
+        if($respuesta > 0){
+            $sql = "update incidencias set observacion = '$observacion', fecha_atendida = NOW(), fecha_modificacion = NOW(), latendida = 1"; 
+            $sql .= $sqlupdate ." where idincidencia = '$idincidencia'";
+            $respuesta = ejecutarConsulta($sql);
+        }else{
+            $respuesta = false;
+        }
+
         return $respuesta;
     }
 }
